187c5a97552bd4b11301f91a1badd37735b38357
Xiaobo.Dong <Xiaobo.Dong@spreadtrum.com>
2017-05-23 19:45:34 +0800

    update project

--- opencl_programming/opencl_performance_optimization/No.1_HelloOpenCL/Android.mk
+++ opencl_programming/opencl_performance_optimization/No.1_HelloOpenCL/Android.mk
@@ -10,7 +10,7 @@ LOCAL_MODULE_TAGS := eng optional tests
 LOCAL_ARM_MODE := arm
 
 LOCAL_C_INCLUDES := $(TOP)/system/core/include/cutils/ \
-			$(LOCAL_PATH)/../include
+			$(LOCAL_PATH)/../../include
 
 # Mark source files as dependent on Android.mk
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
