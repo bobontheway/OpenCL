4d4d66c929a5d2774df7cf4264b5351812eae02e
Xiaobo.Dong <Xiaobo.Dong@spreadtrum.com>
2017-05-25 19:16:47 +0800

    update files

--- opencl_programming/opencl_performance_optimization/No.3_OpenCLTimeCount/Android.mk
+++ opencl_programming/opencl_performance_optimization/No.3_OpenCLTimeCount/Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := OpenCLProgram.cpp
+LOCAL_SRC_FILES := OpenCLTimeCount.cpp
 
-LOCAL_MODULE := opencl_program
+LOCAL_MODULE := opencl_time
 
 LOCAL_SHARED_LIBRARIES := libcutils libutils libOpenCL
 LOCAL_MODULE_TAGS := eng optional tests
