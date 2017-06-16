30a74769970ef1712c1a3dc62f259f6bdde3acfb
Xiaobo.Dong <Xiaobo.Dong@spreadtrum.com>
2017-06-16 15:47:21 +0800

    update project No.1_UMLIterator

---
+++ Java/No.1_UMLIterator/src/BookShelfIterator.java
@@ -0,0 +1,29 @@
+// 迭代书架上的书（对书架上的书进行操作）。在构造函数中，书架作为参数传入
+public class BookShelfIterator implements Iterator {
+	// 书架，以及指向书架上书的下标
+	private BookShelf bookShelf;
+	private int index;
+
+	// 构造对象时初始化私有字段
+	public BookShelfIterator(BookShelf bookShelf) {
+		this.bookShelf = bookShelf;
+		this.index = 0;
+	}
+
+	// 判断书架上是否还有下一本书
+	public boolean hasNext() {
+		if (index < bookShelf.getLength()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// 返回迭代器当前所指向的书，并让迭代器指向下一本书
+	public Object next() {
+		Book book = bookShelf.getBookAt(index);
+		index++;
+		return book;
+	}
+}
+
