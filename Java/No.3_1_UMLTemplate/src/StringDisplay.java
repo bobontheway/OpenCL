3f93393df8b2d8aa978cc23dc324259d0ede558b
Xiaobo.Dong <xiaobo_dong@yeah.net>
2017-06-17 23:46:00 +0800

    add project No.3_1_UMLTemplate

---
+++ Java/No.3_1_UMLTemplate/src/StringDisplay.java
@@ -0,0 +1,29 @@
+public class StringDisplay extends AbstractDisplay {
+	private String string;
+	private int width;
+
+	public StringDisplay(String string) {
+		this.string = string;
+		this.width = string.getBytes().length;
+	}
+
+	public void open() {
+		printLine();
+	}
+
+	public void print() {
+		System.out.println("|" + string + "|");
+	}
+
+	public void close() {
+		printLine();
+	}
+
+	private void printLine() {
+		System.out.print("+");
+		for (int i = 0; i < width; i++) {
+			System.out.print("-");
+		}
+		System.out.println("+");
+	}
+}
